746f74f39aae9e8c998679b7b73d6c065caf40e1
starfishlover62 <jmgillum@nic.edu>
2023-10-27 18:27:30 -0700

    Added basic functions for aliens

--- alien.cpp
+++ alien.cpp
@@ -1,7 +1,33 @@
 #include "alien.h"
 
+int Squid::points = 20;
+std::string Squid::textureFile = "pngegg.png";
+int Squid::textureLocation [4] {60,102,47,47};
+
+
 
 Alien::Alien(){
-    positionX = 0;
-    positionY = 0;
+    mPosition = {0,0};
+    mScale = {1,1};
+}
+
+
+void Squid::draw(sf::RenderTarget& target, sf::RenderStates states) const {
+    target.draw(mSprite, states);
+}
+
+Squid::Squid(){
+    setTexture();
+    mSprite.setScale(mScale);
+    mSprite.setPosition(sf::Vector2f(mPosition));
+}
+
+
+void Squid::setTexture(){
+    if(!mTexture.loadFromFile(textureFile)){
+        std::cout <<"Error opening file" << std::endl;
+        exit(1);
+    }
+    mSprite.setTexture(mTexture);
+    mSprite.setTextureRect(sf::IntRect(textureLocation[0],textureLocation[1],textureLocation[2],textureLocation[3]));
 }
--- alien.h
+++ alien.h
@@ -1,24 +1,51 @@
 #ifndef ALIEN_H
 #define ALIEN_H
 
+#include <string>
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
 class Alien {
 
 public:
     Alien();
 
-    int x() const { return positionX; }
-    int y() const { return positionY; }
+    int x() const { return mPosition.x; }
+    int y() const { return mPosition.y; }
+    sf::Vector2f getLocation() const { return mPosition; }
 
 
+    void update(sf::Event& event, sf::RenderWindow& window);
+    //virtual void draw(sf::RenderTarget& target,sf::RenderStates states) const;
+    virtual void setTexture() = 0;
+    
 
-
-private:
-    int positionX, positionY;
+protected:
+    sf::Vector2f mPosition;
+    sf::Sprite mSprite;
+    sf::Texture mTexture;
+    sf::Vector2f mScale;
 
 
 };
 
 
+
+class Squid : public Alien {
+
+public:
+    Squid();
+
+    virtual void setTexture();
+    virtual void draw(sf::RenderTarget& target,sf::RenderStates states) const;
+
+    static int points;
+    static std::string textureFile; // path to texture file
+    static int textureLocation [4]; // {startx,starty,sizex,sizey}
+
+private:
+
+};
 
 
 #endif
