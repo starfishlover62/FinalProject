538ea53fd73ff923339a90fbc26c46d2075b3611
Josh Gillum <jmgillum@nic.edu>
2023-11-01 15:13:31 -0700

    Moved PlayerName class to its own file

---
+++ playerName.cpp
@@ -0,0 +1,89 @@
+#include "playerName.h"
+
+void PlayerName::setPlayerName(sf::RenderWindow& window){
+
+    // begin enter player name
+    sf::Font font;
+    if (!font.loadFromFile("assets/arial.ttf")) // don't know how to load font from c drive
+    {
+        // Handle font loading error
+        std::cerr << "Error loading font file." << std::endl;
+        exit(1);
+    }
+
+    sf::Text promptForName;
+    promptForName.setString("Player Name: "); // text
+    promptForName.setPosition(200, 300); // position
+    promptForName.setFont(font);
+    promptForName.setCharacterSize(24);
+    promptForName.setFillColor(sf::Color::White);
+
+    sf::Text startTyping;
+    startTyping.setString("(Start Typing)"); // text
+    startTyping.setPosition(375, 300); // position
+    startTyping.setFont(font);
+    startTyping.setCharacterSize(24);
+    startTyping.setFillColor(sf::Color::White);
+
+    sf::Text input; // user input
+    input.setPosition(375, 300); // position
+    input.setFont(font);
+    input.setCharacterSize(24);
+    input.setFillColor(sf::Color::White);
+
+    //std::string mPlayerName;
+    bool pressedEnter = false;
+    bool enteredCharacter = false;
+
+    while (window.isOpen() && !pressedEnter)
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+            else if (event.type == sf::Event::TextEntered)
+            {
+                if (event.text.unicode < 128)
+                {
+                    if (event.text.unicode == 13)
+                    {
+                        pressedEnter = true;
+                    }
+                    else if (event.text.unicode == 8 && mPlayerName.size() > 0)
+                    {
+                        // handle backspace to erase characters
+                        mPlayerName.pop_back();
+                    }
+                    else
+                    {
+                        // handle regular key presses and add them to the inputText
+                        enteredCharacter = true;
+                        mPlayerName += static_cast<char>(event.text.unicode);
+                    }
+                    input.setString(mPlayerName);
+                }
+            }
+        }
+
+        window.clear();
+
+        // show "(Start Typing)" until user types a character
+        if(!enteredCharacter)
+        {
+            window.draw(startTyping);
+        }
+
+        window.draw(promptForName);
+        window.draw(input);
+        window.display();
+    }
+    // player name becomes "No Name" if player doesn't enter a name
+    if(!enteredCharacter)
+    {
+        mPlayerName = "No Name";
+    }
+// end enter player name
+}
---
+++ playerName.h
@@ -0,0 +1,26 @@
+#ifndef PLAYERNAME_H
+#define PLAYERNAME_H
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <string>
+
+class PlayerName
+{
+    public:
+        void setPlayerName(sf::RenderWindow& window);
+
+        void displayPlayerName();
+
+        std::string setName() const { return mPlayerName; }
+
+    private:
+        std::string mPlayerName;
+        sf::Font mFont;
+        bool mFontLoaded;
+        std::string mInput;
+        sf::Text mPromptForName;
+        sf::Text mInputText;
+};
+
+#endif
