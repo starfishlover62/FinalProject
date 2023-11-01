c2d5126b4cd992794fe377a080916b94187d279d
Josh Gillum <jmgillum@nic.edu>
2023-11-01 15:13:47 -0700

    Moved GameBoard class to its own file

---
+++ board.cpp
@@ -0,0 +1,36 @@
+#include "board.h"
+
+
+Gameboard::Gameboard(const std::string& playerName, int initialScore) : mPlayerName(playerName), mScore(initialScore)
+{
+
+    mFont.loadFromFile("assets/arial.ttf"); // don't know how to load font from c drive
+    mNameText.setFont(mFont);
+    mNameText.setCharacterSize(24);
+    mNameText.setFillColor(sf::Color::White);
+    mNameText.setPosition(20, 20);
+    mNameText.setString(playerName);
+
+    updateScoreText();
+    mScoreText.setPosition(600, 20);
+}
+
+void Gameboard::increaseScore(int increaseBy)
+{
+    mScore += increaseBy;
+    updateScoreText();
+}
+
+void Gameboard::draw(sf::RenderWindow& window)
+{
+    window.draw(mNameText);
+    window.draw(mScoreText);
+}
+
+void Gameboard::updateScoreText()
+{
+    mScoreText.setFont(mFont);
+    mScoreText.setCharacterSize(24);
+    mScoreText.setFillColor(sf::Color::White);
+    mScoreText.setString("Score: " + std::to_string(mScore));
+}
