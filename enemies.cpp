2e6c10924ba1729cfa9b5b15158295fc7479be02
starfishlover62 <jmgillum@nic.edu>
2023-11-07 19:06:18 -0800

    Constructor can detect a formation

--- enemies.cpp
+++ enemies.cpp
@@ -1,1 +1,20 @@
 #include "enemies.h"
+
+
+Enemies::Enemies(){
+    numRows = 5;
+    numPerRow = 11;
+    formation = new int[numRows] {squid,crab,crab,jellyfish,jellyfish};
+    for(int i = 0; i < numRows; ++i){
+        for(int j = 0; j < numPerRow; ++j){
+            if(formation[i] == squid){
+                std::cout << i << ": squid" << std::endl;
+            } else if(formation[i] == crab){
+                std::cout << i << ": crab" << std::endl;
+            }  else if(formation[i] == jellyfish){
+                std::cout << i << ": jellyfish" << std::endl;
+            }
+        }
+    }
+    delete formation;
+}
--- enemies.h
+++ enemies.h
@@ -1,16 +1,38 @@
-#ifndef ALIEN_H
-#define ALIEN_H
+#ifndef ENEMIES_H
+#define ENEMIES_H
 
 #include <string>
 #include <SFML/Graphics.hpp>
+#include <vector>
+
+#include "alien.h"
+
+
+
+
+#include <iostream>
+
+
+
 
 class Enemies{
 
 public:
+    enum enemyType {squid, crab, jellyfish};
+    Enemies();
 
     
 
 protected:
+
+    std::vector<Alien *> aliens;
+
+    int numRows;
+    int numPerRow;
+    int * formation;
+    
+
+
 
 };
 
