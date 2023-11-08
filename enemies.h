db83c5b3928ecd955b4f2dfc8c25b2fa83112846
starfishlover62 <jmgillum@nic.edu>
2023-11-07 18:51:20 -0800

    Created outline

---
+++ enemies.h
@@ -0,0 +1,18 @@
+#ifndef ALIEN_H
+#define ALIEN_H
+
+#include <string>
+#include <SFML/Graphics.hpp>
+
+class Enemies{
+
+public:
+
+    
+
+protected:
+    int mScore;
+    std::string mName;
+};
+
+#endif
