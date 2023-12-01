ca47aee1d3aac0c2f2383f9cb694c5d1300654de
starfishlover62 <jmgillum@nic.edu>
2023-11-30 22:32:44 -0800

    Comments and removed cout statements

--- animation.cpp
+++ animation.cpp
@@ -1,53 +1,90 @@
+/**
+ * @file animation.cpp
+ * @author Josh Gillum
+ * @brief Function implementations for animation class
+ * @version 0.1
+ * @date 2023-11-30
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
 #include "animation.h"
 
-
+/**
+ * @brief Default animation constructors
+ * 
+ */
 Animation::Animation(){
-    mNumFrames = -1;
+    // Values of negative one indicate that that they have not been set
+    mNumFrames = -1;    
     mCurrentFrame = -1;
-    mRepeatAnimation = false;
+    mRepeatAnimation = false; // Animation will not loop by default
+    mAutoUpdate = false; // Animation will be manually cycled by default
     mFinished = false;
-    mTimePerAnimation = sf::Time::Zero;
+
+    // Time values of zero indicate that they have not been set
+    mTimePerAnimation = sf::Time::Zero; 
     mTimePerFrame = sf::Time::Zero;
     mTimeSinceLastUpdate = sf::Time::Zero;
-    mManualUpdate = true;
-
 }
 
 
+/**
+ * @brief Appends a frame to the end of the animation. Currently, there is no way to reorder frames,
+ * so they must be added in the intended display order
+ * 
+ * @param startX, starting x coordinate of the frame
+ * @param startY, starting y coordinate of the frame
+ * @param endX, ending x coodinate of the frame
+ * @param endY, ending y coordinate of the frame
+ */
 void Animation::addFrame(int startX, int startY, int endX, int endY){
-    mFrames.push_back(frame(startX,startY,endX,endY));
-    if(mNumFrames <= 0){
+    mFrames.push_back(frame(startX,startY,endX,endY)); // Adds frame to vector
+    if(mNumFrames <= 0){ // Initialized numFrames and currentFrame if this is the first frame
         mNumFrames = 1;
         mCurrentFrame = 0;
     } else {
         ++mNumFrames;
     }
-    updateTimePerFrame();
+    updateTimePerFrame(); // Updates the time per frame, necessary since now more frames must be shown in the same amount of time
 }
 
+
+/**
+ * @brief Sets the amount of time that the animation should last
+ * 
+ * @param time, the total time to elapse for one complete animation
+ */
 void Animation::setAnimationTime(sf::Time time){
-    mManualUpdate = false;
+    mAutoUpdate = true; // Because a time is specified for the animation to elapse, it is implicitly set to auto update
     mTimePerAnimation = time;
     if(mNumFrames > 0){ updateTimePerFrame(); }
 }
 
+
+/**
+ * @brief Updates the frame based on mAutoUpdate's value. If mAutoUpdate is true, will change the frame every 
+ * mTimePerFrame seconds. If mAutoUpdate is false, will change the frame every time the function is called.
+ * 
+ * @return true, the frame was changed
+ * @return false, the frame was not changed (The end of a non-looping animation was reached)
+ */
 bool Animation::updateFrame(){
-    if(!valid()) { throw UnInitializedAnimation(); }
-    if(!mManualUpdate){
-        // std::cout << "Auto update frame" << std::endl;
-        mTimeSinceLastUpdate += mClock.restart();
-        bool updated = false;
-        while (mTimeSinceLastUpdate > mTimePerFrame){
+    if(!valid()) { throw UnInitializedAnimation(); } // Throws exception if the animation has not been fully initialized
+    if(mAutoUpdate){
+        mTimeSinceLastUpdate += mClock.restart(); // Stores the time elapsed since the frame was last changed
+        bool updated = false; // Tracks whether the frame was changed
+        while (mTimeSinceLastUpdate > mTimePerFrame){ // Loops for the number of frames elapsed since the frame was last changed
             mTimeSinceLastUpdate -= mTimePerFrame;
-            if(!cycleFrames()){
-                // std::cout << "Not cycle frames" << std::endl;
+            if(!cycleFrames()){ // Cycles to the next frame in the animation
                 return false;
             }
             updated = true;
         }
 
         return updated;
-    } else {
+    } else { // If mAutoUpdate is false (manual updating is in effect)
         if(!cycleFrames()){
             return false;
         }
@@ -56,18 +93,22 @@ bool Animation::updateFrame(){
 }
 
 
+/**
+ * @brief Moves the animation to the next frame.
+ * 
+ * @return true, the frame was successfully changed
+ * @return false, the frame was not successfully changed (the end of a non-looping animation has been reached)
+ */
 bool Animation::cycleFrames(){
     if(!valid()) { throw UnInitializedAnimation(); }
     if(!mFinished){
         ++mCurrentFrame;
-        if(!mRepeatAnimation){
-            // std::cout << "Not repeat" << std::endl;
-            if (mCurrentFrame == mNumFrames){ 
-                // std::cout << "last frame" << std::endl;
+        if(!mRepeatAnimation){ // If the animation does not loop
+            if (mCurrentFrame == mNumFrames){ // Detects whether the end of the animation has been reached
                 mFinished = true; 
                 return false;
             }
-        } else {
+        } else { // Otherwise loops the frame
             mCurrentFrame %= mNumFrames;
         }
         return true;
@@ -75,6 +116,12 @@ bool Animation::cycleFrames(){
     return false;
 }
 
+
+/**
+ * @brief Returns a rectangle for the current frame
+ * 
+ * @return sf::IntRect (top-leftX, top-leftY, width, height)
+ */
 sf::IntRect Animation::getFrame() const {
     if(!valid()) { throw UnInitializedAnimation(); }
     return(sf::IntRect(mFrames[mCurrentFrame].startX,
@@ -84,16 +131,28 @@ sf::IntRect Animation::getFrame() const {
 }
 
 
+/**
+ * @brief Checks whether the animation has been fully initialized
+ * 
+ * @return true, the animation has been fully initialized and can be used
+ * @return false, the animation has not been fully initialized. Attempting to use it will cause
+ * an UnInitializedAnimation exception
+ */
 bool Animation::valid() const {
     if(mNumFrames == -1) { return false; }
     if(mCurrentFrame == -1) { return false; }
-    if(!mManualUpdate){
+    if(mAutoUpdate){
         if(mTimePerAnimation == sf::Time::Zero) { return false; }
         if(mTimePerFrame == sf::Time::Zero) { return false; }
     }
     return true;
 }
 
+
+/**
+ * @brief Resets the animation. Delets all frames and resets time values
+ * 
+ */
 void Animation::clear() {
     mFrames.clear();
     mNumFrames = -1;
--- animation.h
+++ animation.h
@@ -1,13 +1,24 @@
-// Class to handle different animations
+/**
+ * @file animation.h
+ * @author Josh Gillum
+ * @brief Class definition for animation class
+ * @version 0.1
+ * @date 2023-11-30
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
 
 #ifndef ANIMATION_H
 #define ANIMATION_H
 
-#include <SFML/Graphics.hpp>
 #include <vector>
+#include <SFML/Graphics.hpp>
 
-#include <iostream>
-
+/**
+ * @brief Represents a single frame of an animation. Stores the top-left and bottom-right coordinates of the frame.
+ * 
+ */
 struct frame {
     int startX;
     int startY;
@@ -26,56 +37,56 @@ class Animation {
 
     public:
 
+        // Exception Class
+        class UnInitializedAnimation{}; // Thrown when animation is used before it has been fully initialized (some values were not set)
+
+        // Constructor
         Animation();
 
-        void addFrame(int startX, int startY, int endX, int endY);
-        void setAnimationTime(sf::Time time);
-        void setAnimationRepeat(bool repeat) { mRepeatAnimation = repeat; }
-        void enableManualUpdate() { mManualUpdate = true; }
-        void disableManualUpdate() { mManualUpdate = false; }
+        // Getters
+        sf::IntRect getFrame() const; // Returns the current frame information
 
-        void resetClock() { mClock.restart(); }
-
-        void clear();
-
-        sf::IntRect getFrame() const;
-
-        bool valid() const;
+        bool valid() const; // Returns whether the animation has been fully initialized
         
+        // Returns the size of the current frame
         sf::Vector2f size() const { return sf::Vector2f(mFrames[mCurrentFrame].endX - mFrames[mCurrentFrame].startX,
                                                  mFrames[mCurrentFrame].endY - mFrames[mCurrentFrame].startY); }
 
-        int sizeX() const { return mFrames[mCurrentFrame].endX - mFrames[mCurrentFrame].startX; }
-        int sizeY() const { return mFrames[mCurrentFrame].endY - mFrames[mCurrentFrame].startY; }
+        int sizeX() const { return mFrames[mCurrentFrame].endX - mFrames[mCurrentFrame].startX; } // Returns the width of the current frame
+        int sizeY() const { return mFrames[mCurrentFrame].endY - mFrames[mCurrentFrame].startY; } // Returns the height of the current frame
 
-        int numFrames() const { return mNumFrames; }
-        bool repeated() const { return mRepeatAnimation; }
-        bool complete() const { return mFinished; }
+        int numFrames() const { return mNumFrames; }        // Returns the number of frames in the animation
+        bool repeated() const { return mRepeatAnimation; }  // Returns true if the animation loops, false if it doesnt
+        bool complete() const { return mFinished; }         // Returns whether the end of the animation has been reached
 
-        bool updateFrame();
+        // Updating
+        void addFrame(int startX, int startY, int endX, int endY); // Adds a frame to the end of the animation
+        void setAnimationTime(sf::Time time); // Change the total time that the animation will last for
+        bool updateFrame(); // Cycles to the next frame of the animation, depends on auto or manual updating
+        
+        void enableAnimationRepeat() { mRepeatAnimation = true; }   // Turns on looping of the animation
+        void disableAnimationRepeat() { mRepeatAnimation = false; } // Disables looping of the animation
+        void enableAutoUpdate() { mAutoUpdate = true; }             // Animation will change frames based on time elapsed
+        void disableAutoUpdate() { mAutoUpdate = false; }           // Animation will change frames based on calls to updateFrame
 
-        class UnInitializedAnimation{};
+        void resetClock() { mClock.restart(); } // Sets the clock back to zero
+        void clear(); // Resets all values of the animation. Erases all frames
 
     protected:
 
-        void updateTimePerFrame() { mTimePerFrame = sf::seconds(mTimePerAnimation.asSeconds() / mNumFrames); }
-        bool cycleFrames();
+        void updateTimePerFrame() { mTimePerFrame = sf::seconds(mTimePerAnimation.asSeconds() / mNumFrames); } // Updates mTimePerFrame
+        bool cycleFrames(); // Cycles to the next frame of the animation
     
-        int mNumFrames;
-        int mCurrentFrame; // Current frame, starts at 0
-        bool mRepeatAnimation;
-        bool mFinished;
-        bool mManualUpdate;
-        sf::Time mTimePerAnimation;
-        sf::Time mTimePerFrame;
-        sf::Time mTimeSinceLastUpdate;
-        sf::Clock mClock;
-        std::vector<frame> mFrames;
-
-
-    private:
-
-
-
+        int mNumFrames;                 // Number of frames in animation
+        int mCurrentFrame;              // Current frame number, starts at 0
+        bool mRepeatAnimation;          // True if animation loops, false if it doesn't
+        bool mFinished;                 // True if the last frame of a non-looping animation has been cycled past
+        bool mAutoUpdate;               // True if frame cycles are based on time, false if based on function callse
+        sf::Time mTimePerAnimation;     // Time to elapse for total animation
+        sf::Time mTimePerFrame;         // Time to elapse for each frame
+        sf::Time mTimeSinceLastUpdate;  // Time elapsed since last frame change
+        sf::Clock mClock;               // Stores elapsed time
+        std::vector<frame> mFrames;     // Stores all of the frames
 };
+
 #endif
