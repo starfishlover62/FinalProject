d04cb52375839ae53cc5a824f14a0b01d3e94bc1
starfishlover62 <jmgillum@nic.edu>
2023-12-04 14:51:26 -0800

    Cleaned up

--- alien.h
+++ alien.h
@@ -30,56 +30,26 @@ class Alien : public Object{
 
 public:
 
-    // Exception Classes
-    // class loadTextureError{ // Used for when a texture can not be loaded from file
-    //     public:
-    //         loadTextureError(){ path = "Unspecified"; }
-    //         loadTextureError(std::string path){ this->path = path;}
-    //         std::string path;
-    // }; 
+    // Exception Class
     class InvalidMove{}; // Used when no direction is specified for moving
 
     // Constructor
     Alien(sf::Vector2f position = sf::Vector2f(0,0));
 
     // Getters
-    // sf::Vector2f position() const { return mPosition; } // Returns the position of the sprite as a vector
-    // int x() const { return mPosition.x; }               // Returns the position of the sprite on the x-axis
-    // int y() const { return mPosition.y; }               // Returns the position of the sprite on the y-axis
-    // sf::Vector2f size() const;  // Returns the size of the sprite as a vector
-    // int sizeX() const;          // Returns the width of the sprite
-    // int sizeY() const;          // Returns the height of the sprite
     bool dead() const { return mDead; } // Returns true if the alien is dead, false if living
     bool readyForDeletion() const { return mAnimation.complete(); } // Returns true if the explosion animation is complete
     int points() const { return mPoints; } // Returns the number of points the alien is worth
 
-    // Drawing
-    // virtual void draw(sf::RenderTarget& target) const { target.draw(mSprite); } // Draws the alien
-    // virtual void draw(sf::RenderTarget& target,sf::RenderStates states) const { target.draw(mSprite,states); } // Draws the alien
-
-    // Updating
-    // void setTextureFile(std::string path) { mTextureFile = path; } // Sets the path to the texture file
-    // void setPosition(sf::Vector2f position); // Sets the position of the sprite    
+    // Moving
     void moveX(float distance, int direction = 1);  // Moves the sprite along the x-axis
     void moveY(float distance, int direction = 1);  // Moves the sprite along the y-axis
     virtual void kill(); // Kills the alien, updates to the explosion animation
-    // virtual bool cycleFrames(); // Advances the frame
     
 
 protected:
-    // sf::Vector2f mPosition;     // Position of the alien
-    // sf::Sprite mSprite;         // Sprite of the alien
-    // sf::Texture mTexture;       // Texture for the sprite
-    // sf::Vector2f mScale;        // How much to scale the sprite by
-
-    // std::string mTextureFile;   // Path to the texture file
-    // Animation mAnimation;       // Animation for the alien
-
     int mPoints;                // Number of points alien is worth
     bool mDead;                 // True if dead, false if alive
-
-    // virtual void setTexture();  // Sets the texture for the sprite
-
 };
 
 
