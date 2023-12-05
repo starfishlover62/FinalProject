f8ec7eeb931c923349351898fac6c8a423b110ef
starfishlover62 <jmgillum@nic.edu>
2023-12-04 20:00:32 -0800

    Created player class

---
+++ player.cpp
@@ -0,0 +1,170 @@
+#include "player.h"
+
+Player::Player(int screenWidth,int screenHeight){
+    mScreenWidth = screenWidth;
+    mScreenHeight = screenHeight;
+    mTank = new Tank(mScreenWidth);
+    mLives = 3;
+
+    for(int i = 0; i < mLives; ++i){
+        mTankLives.push_back(new Tank(mScreenWidth,{690+(50.f*i), 10}));
+
+        // mTankLives[i]->setOrigin(-380-(100*i), 1485.f);
+
+        float initialTankLifeScale = 0.5f; // Adjust this value as needed
+        mTankLives[i]->setScale({initialTankLifeScale, initialTankLifeScale});
+    }
+    mBullet = new FriendlyBullet({mTank->x(),mTank->y()},-10.0);
+
+    mBulletVisible = false;
+    mHud = new Gameboard("p1",0);
+
+    mMoving = false;
+    mVelocity = 2;
+    mTimeSinceLastUpdate = sf::Time::Zero;
+    mTimePerUpdate = sf::seconds(0.01);
+    mClock.restart();
+}
+
+
+Player::~Player(){
+    if(mTank != nullptr){
+        delete mTank;
+    }
+    if(mHud != nullptr){
+        delete mHud;
+    }
+    for(unsigned i = 0; i < mTankLives.size(); ++i){
+        if(mTankLives[i] != nullptr){
+            delete mTankLives[i];
+        }
+    }
+    if(mBullet != nullptr){
+        delete mBullet;
+    }
+}
+
+void Player::draw(sf::RenderTarget& target) const {
+    mTank->draw(target);
+    mHud->draw(target);
+    if(mBulletVisible){
+        mBullet->draw(target);
+    }
+    // for(unsigned i = 0; i < mTankLives.size(); ++i){
+    //     mTankLives[i]->draw(target);
+    // }
+    std::for_each(mTankLives.begin(),mTankLives.end(),[&target](const Tank* t){t->draw(target);});
+}
+
+void Player::draw(sf::RenderTarget& target,sf::RenderStates states) const {
+    mTank->draw(target,states);
+    mHud->draw(target,states);
+    if(mBulletVisible){
+        mBullet->draw(target,states);
+    }
+    // for(unsigned i = 0; i < mTankLives.size(); ++i){
+    //     mTankLives[i]->draw(target,states);
+    // }
+    std::for_each(mTankLives.begin(),mTankLives.end(),[&target,&states](const Tank* t){t->draw(target,states);});
+}
+
+void Player::moving() { 
+    if(!mMoving){
+        mMoving = true; 
+        mClock.restart();
+        mTimeSinceLastUpdate = sf::Time::Zero;
+    }
+}
+
+void Player::noMoving() { 
+    if(mMoving){
+        mMoving = false; 
+        mClock.restart();
+        mTimeSinceLastUpdate = sf::Time::Zero;
+    }
+}
+
+void Player::update(){
+    if(mBulletVisible){
+        mBullet->update();
+        if(mBullet->y() < 5){
+            mBulletVisible = false;
+        }
+    }
+}
+
+
+
+void Player::update(sf::Event& event, sf::RenderWindow& window){
+    if(event.type == sf::Event::KeyReleased){
+        if((event.key.code == sf::Keyboard::Right || event.key.code == sf::Keyboard::D) ||
+           (event.key.code == sf::Keyboard::Left || event.key.code == sf::Keyboard::A)){
+            noMoving();
+        }
+    }
+}
+
+
+void Player::moveTankLeft(){
+    mTank->moveTankLeft();
+}
+
+
+void Player::moveTankRight(){
+    mTank->moveTankRight();
+}
+
+
+void Player::move(bool right){
+    mTimeSinceLastUpdate += mClock.restart();
+    while(mTimeSinceLastUpdate > mTimePerUpdate){
+        mTimeSinceLastUpdate -= mTimePerUpdate;
+        float pos = mTank->x();
+        if(right){
+            if(pos + mTank->sizeX() + 2 < mScreenWidth){
+                mTank->moveX(mVelocity);
+            }
+        } else {
+            if(pos > 2){
+                mTank->moveX(-mVelocity);
+            }
+        }
+    }
+}
+
+
+bool Player::shoot(){
+    if(!mBulletVisible){
+        // std::cout << "shoot" << std::endl;
+        mBulletVisible = true;
+        mBullet->setPosition({mTank->x() + (mTank->sizeX()/2), mTank->y()});
+        mBullet->reset();
+        return true;
+    }
+    return false;
+}
+
+const FriendlyBullet* Player::bulletPtr() const{
+    if(mBulletVisible){
+        return mBullet;
+    } else {
+        return nullptr;
+    }
+}
+
+const Tank* Player::tankPtr() const{
+    return mTank;
+}
+
+
+bool Player::loseLife() {
+    if(mLives > 0){
+        --mLives;
+        if(mTankLives[mTankLives.size()-1] != nullptr){
+            delete mTankLives[mTankLives.size()-1];
+            mTankLives.pop_back();
+        }
+        return true;
+    }
+    return false;
+}
---
+++ player.h
@@ -0,0 +1,59 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "tank.h"
+#include "gameboard.h"
+#include "bullet.h"
+#include <SFML/Graphics.hpp>
+#include <vector>
+
+class Player : public sf::Drawable {
+    public:
+        Player(int screenWidth = 0,int screenHeight = 0);
+        ~Player();
+
+        virtual void draw(sf::RenderTarget& target) const;
+        virtual void draw(sf::RenderTarget& target,sf::RenderStates states) const;
+        void update();
+        void update(sf::Event& event, sf::RenderWindow& window);
+
+        const FriendlyBullet* bulletPtr() const;
+        const Tank* tankPtr() const;
+
+        void moveTankLeft();
+        void moveTankRight();
+
+        bool loseLife();
+
+        void moving();
+        void noMoving();
+        
+
+        void hideBullet() { mBulletVisible = false; }
+        void updateScore(int points) { mHud->increaseScore(points); }
+
+        void move(bool right = true);
+        int x() const { return mTank->x(); }
+        int y() const { return mTank->y(); }
+
+        bool shoot();
+
+    protected:
+        int mScreenWidth, mScreenHeight;
+        Tank* mTank;
+        int mLives;
+        double mVelocity;
+        Gameboard* mHud;
+        std::vector<Tank*> mTankLives;
+        FriendlyBullet* mBullet;
+
+        bool mBulletVisible;
+        bool mMoving;
+
+        sf::Clock mClock;
+        sf::Time mTimeSinceLastUpdate;
+        sf::Time mTimePerUpdate;
+
+};
+
+#endif
